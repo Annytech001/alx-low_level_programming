93cb6fe935988c0cb2607e9914540705b8164825
Annytech001 <godlybassey101@gmail.com>
2022-12-12 15:23:01 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - print text to standard error
+* Return: 0
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	return (1);
+}
