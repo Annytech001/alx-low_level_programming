a9e778323b5f17d1e3d696cdb63c4c7539f1144e
Annytech001 <godlybassey101@gmail.com>
2022-12-22 13:42:02 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,10 +1,10 @@
 #include "main.h"
+
 /**
  * _strncpy - copy a string
  * @dest: input value
  * @src: input value
  * @n: input value
- *
  * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
