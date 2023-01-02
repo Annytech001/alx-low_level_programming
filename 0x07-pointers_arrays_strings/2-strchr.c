a5b6a914a60fdf93460c759c5a80705ac7ea27bd
Annytech001 <godlybassey101@gmail.com>
2023-01-02 12:43:08 -0800

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,9 @@
-#include "main h"
-
+#include "main.h"
 /**
- *
- *_strchr - Entry point
- *@s: input
- *@c: input
- *Return: Always 0 (Sucess)
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
  */
 char *_strchr(char *s, char c)
 {
