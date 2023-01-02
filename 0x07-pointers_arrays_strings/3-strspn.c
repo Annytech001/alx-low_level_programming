cb72aa2a4bf07a107260b9c353972b3ce1a70946
Annytech001 <godlybassey101@gmail.com>
2023-01-02 13:20:45 -0800

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - Lenght of the prefix substring
+ * @s: Searched string
+ * @accept: Measured prefix
+ * Return: Number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+	int r;
+
+	while (*s)
+	{
+		for (r = 0; accept[r]; r++)
+		{
+			if (*s == accept[r])
+			{
+				n++;
+				break;
+			}
+			else if (accept[r + 1] == '\0')
+				return (bytes);
+		}
+		s++;
+	}
+	return (bytes);
+}
